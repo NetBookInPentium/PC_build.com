a22b3d5781244b2c2772d5865b951bf2c5e3ad2f
Александр <qqwertyssssb@gmail.com>
2024-04-20 23:25:04 +0300

    Add folders

--- pc_build_web/pc_build_web/obj/Debug/net8.0/pc_build_web.AssemblyInfo.cs
+++ pc_build_web/pc_build_web/obj/Debug/net8.0/pc_build_web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pc_build_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b378b4ac3885d13162ac5c328f64eaf24d682a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("pc_build_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pc_build_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
