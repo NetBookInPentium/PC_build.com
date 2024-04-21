0252468a36188b887a17ba9cfead2fcbbd5307df
Александр <qqwertyssssb@gmail.com>
2024-04-22 01:13:38 +0300

    Add frontend

--- pc_build_web/pc_build_web/obj/Debug/net8.0/pc_build_web.AssemblyInfo.cs
+++ pc_build_web/pc_build_web/obj/Debug/net8.0/pc_build_web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pc_build_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73920013bc601e4362fdb6372af6d6f13c9513f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b758d422d9e862257b3b5168c9af5a6dfd6fde0")]
 [assembly: System.Reflection.AssemblyProductAttribute("pc_build_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pc_build_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
